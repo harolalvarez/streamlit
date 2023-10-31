aee3435ba0176313238b7384ff9fdced4e5428f0
harolalvarez <130994546+harolalvarez@users.noreply.github.com>
2023-10-31 17:50:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.write("hola,*saludos desde colombia*")
 x=st.slider("selecciona un valor de la barra")
-st.write(x, "su cuadrado es: "x*2)
+st.write(x, "su cuadrado es: ", (x*2))
