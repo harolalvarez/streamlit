c28235f332490d26c494b8028ecc5b5067b8feda
harolalvarez <130994546+harolalvarez@users.noreply.github.com>
2023-10-31 18:11:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,52 @@
 import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
-st.write("hola,*saludos desde colombia*")
-x=st.slider("selecciona un valor de la barra")
-st.write(x, "su cuadrado es: ", (x*2))
+# Cargar el conjunto de datos
+@st.cache
+def load_data():
+    data = pd.read_csv("data.csv")
+    return data
+
+data = load_data()
+
+# Título de la aplicación
+st.title('Plataforma de Evaluación de Riesgos Ambientales en Palmira')
+
+# Análisis de Riesgos
+st.header('Análisis de Riesgos')
+
+# Selecciona características y variable objetivo
+features = st.multiselect("Selecciona características:", data.columns)
+target_variable = st.selectbox("Selecciona la variable objetivo:", data.columns)
+
+X = data[features]
+y = data[target_variable]
+
+# Dividir los datos en conjuntos de entrenamiento y prueba
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Entrenamiento del modelo
+st.subheader('Entrenamiento del Modelo')
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
+# Evaluar el modelo
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+st.write(f'Precisión del modelo: {accuracy}')
+
+# Mostrar resultados, simulación de escenarios, recomendaciones, etc.
+# Agrega secciones adicionales según tus necesidades
+
+# Visualización de datos
+st.header('Visualización de Datos')
+st.write(data)
+
+# Pregunta predictiva
+st.header('Pregunta Predictiva')
+st.write("¿Cuál es el riesgo de eventos ambientales adversos en Palmira y cómo pueden mitigirse a través de medidas preventivas, regulaciones y cambios en la infraestructura?")
+
+# Nota: Extiende y personaliza este código según tus necesidades específicas
