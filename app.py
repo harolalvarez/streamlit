9115bacd50b88f9156abf8ddc7bbb7c132c6e0bd
harolalvarez <130994546+harolalvarez@users.noreply.github.com>
2023-11-03 13:20:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,59 +1,54 @@
+# Importar las bibliotecas necesarias
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
-
-# Cargar el dataset
-df = pd.read_csv("Manejo_residuos_peligrosos_Palmira_preprocessed.csv")
-
-# Seleccionar las características
-X = df[["tipo_identificacion", "tipo_clase_sujeto", "comuna", "barrio", "fecha_programada", "fecha_ejecutada", "tipo", "fuente", "tiempo"]]
-y = df["de_cumplimiento"]
-
-# Entrenar el modelo
-model = DecisionTreeClassifier()
-model.fit(X, y)
-
-# Guardar el modelo
-model.save("model.pkl")
-
-# Streamlit
+import pickle
 import streamlit as st
 
+# Importar el dataset
+df = pd.read_csv("Manejo_residuos_peligrosos_Palmira_preprocessed.csv")
+
+# Eliminar valores faltantes
+df.dropna(inplace=True)
+
+# Codificar variables categóricas
+df["tipo_identificacion_del_sujeto"] = df["tipo_identificacion_del_sujeto"].astype("category")
+df["tipo_clase_de_sujeto"] = df["tipo_clase_de_sujeto"].astype("category")
+
+# Normalizar los datos
+df["comuna"] = df["comuna"]/df["comuna"].max()
+df["barrio"] = df["barrio"]/df["barrio"].max()
+
+# Dividir el conjunto de datos en conjuntos de entrenamiento y prueba
+X_train, X_test, y_train, y_test = train_test_split(df, df["de_cumplimiento"], test_size=0.25)
+
+# Entrenar el modelo
+clf = DecisionTreeClassifier()
+clf.fit(X_train, y_train)
+
+# Evaluar el modelo
+score = clf.score(X_test, y_test)
+print(score)
+
+# Guardar el modelo
+pickle.dump(clf, open("model.pkl", "wb"))
+
 # Cargar el modelo
-model = pickle.load(open("model.pkl", "rb"))
+clf = pickle.load(open("model.pkl", "rb"))
 
-# Función para predecir
-def predict(X):
-  return model.predict(X)
+# Crear la aplicación de Streamlit
+st.title("Predicción de incumplimiento de normas de manejo de residuos peligrosos")
 
-# Mostrar el modelo
-st.title("Modelo de predicción de cumplimiento de residuos peligrosos")
-st.write("Este modelo predice si un sujeto cumplirá con los requisitos de manejo de residuos peligrosos.")
-
-# Entrada de datos
-tipo_identificacion = st.selectbox("Tipo de identificación", df["tipo_identificacion"].unique())
-tipo_clase_sujeto = st.selectbox("Tipo de clase de sujeto", df["tipo_clase_sujeto"].unique())
+# Seleccionar los datos de entrada
+tipo_identificacion_del_sujeto = st.selectbox("Tipo de identificación del sujeto", df["tipo_identificacion_del_sujeto"].unique())
+tipo_clase_de_sujeto = st.selectbox("Tipo de clase de sujeto", df["tipo_clase_de_sujeto"].unique())
 comuna = st.selectbox("Comuna", df["comuna"].unique())
 barrio = st.selectbox("Barrio", df["barrio"].unique())
 fecha_programada = st.date_input("Fecha programada")
-fecha_ejecutada = st.date_input("Fecha ejecutada")
-tipo = st.selectbox("Tipo", df["tipo"].unique())
-fuente = st.selectbox("Fuente", df["fuente"].unique())
-tiempo = st.number_input("Tiempo")
+tipo = st.selectbox("Tipo de visita", df["tipo"].unique())
+fuente = st.selectbox("Fuente de los residuos peligrosos", df["fuente"].unique())
 
-# Predecir
-X = pd.DataFrame({
-  "tipo_identificacion": [tipo_identificacion],
-  "tipo_clase_sujeto": [tipo_clase_sujeto],
-  "comuna": [comuna],
-  "barrio": [barrio],
-  "fecha_programada": [fecha_programada],
-  "fecha_ejecutada": [fecha_ejecutada],
-  "tipo": [tipo],
-  "fuente": [fuente],
-  "tiempo": [tiempo]
-})
+# Predecir el cumplimiento
+prediccion = clf.predict([[tipo_identificacion_del_sujeto, tipo_clase_de_sujeto, comuna, barrio, fecha_programada, tipo, fuente]])[0]
 
-y_pred = predict(X)
-
-# Salida
-st.write("El sujeto tiene un {:.0%} de probabilidad de cumplir con los requisitos.".format(y_pred[0]))
+# Mostrar el resultado
+st.write("El riesgo de incumplimiento es de", prediccion)
