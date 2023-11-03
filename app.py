7fa4f9a4f9c42f25f10376f2ce294afc4d242b8b
harolalvarez <130994546+harolalvarez@users.noreply.github.com>
2023-11-03 14:15:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,23 +2,20 @@ import streamlit as st
 import pandas as pd
 
 # Importar el conjunto de datos
-df = pd.read_csv("Manejo_residuos_peligrosos_Palmira_preprocessed.csv")
+df = pd.read_csv("https://www.datos.gov.co/api/views/39bi-a35k/rows.csv?accessType=DOWNLOAD")
 
 # Verificar si la columna existe
 if "tipo_clase_sujeto" in df.columns:
-    st.bar_chart(df["tipo_clase_sujeto"].value_counts())
+    # Verificar si la columna tiene datos
+    if df["tipo_clase_sujeto"].notna().any():
+        # Agrupar el conjunto de datos por comuna y calcular el número de visitas por tipo de sujeto
+        df_grouped = df.groupby("comuna")["tipo_clase_sujeto"].value_counts()
+
+        # Mostrar el mapa
+        st.map(df_grouped)
+    else:
+        # Mostrar un mensaje de error si la columna no tiene datos
+        st.write("La columna `tipo_clase_sujeto` no tiene datos.")
 else:
     # Mostrar un mensaje de error si la columna no existe
     st.write("La columna `tipo_clase_sujeto` no existe en el conjunto de datos.")
-
-# Mostrar un resumen del conjunto de datos
-st.write(df.describe())
-
-# Mostrar un gráfico de barras de la cantidad de visitas por comuna
-st.map(df.groupby("comuna")["tipo_clase_sujeto"].value_counts())
-
-# Mostrar un gráfico de líneas de la cantidad de visitas por fecha
-st.line_chart(df.groupby("fecha_program")["tipo_clase_sujeto"].value_counts())
-
-# Mostrar una tabla de los datos de una visita específica
-st.table(df.loc[df["tipo_identificacion"] == "NIT"])
