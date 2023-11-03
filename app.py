45aaf2d2107ebab08814df6833ec6196e10e01d6
harolalvarez <130994546+harolalvarez@users.noreply.github.com>
2023-11-03 13:34:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pickle
 import streamlit as st
 
 # Importar el dataset
-df = pd.read_csv("Manejo_residuos_peligrosos_Palmira_preprocessed.csv")
+df = pd.read_csv("Manejo_residuos_peligrosos_Palmira_preprocessed.csv", dtype={"de_cumplimiento": "category"})
 
 # Eliminar valores faltantes
 df.dropna(inplace=True)
