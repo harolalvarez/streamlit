ac78ab8e7434a1471b07422a822c928fff49f886
harolalvarez <130994546+harolalvarez@users.noreply.github.com>
2023-11-03 14:12:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,19 @@ import streamlit as st
 import pandas as pd
 
 # Importar el conjunto de datos
-df = pd.read_csv("Manejo_residuos_peligrosos_Palmiraa.csv")
+df = pd.read_csv("https://www.datos.gov.co/api/views/39bi-a35k/rows.csv?accessType=DOWNLOAD")
+
+# Verificar si la columna existe
+if "tipo_clase_sujeto" in df.columns:
+    st.bar_chart(df["tipo_clase_sujeto"].value_counts())
+else:
+    # Mostrar un mensaje de error si la columna no existe
+    st.write("La columna `tipo_clase_sujeto` no existe en el conjunto de datos.")
 
 # Mostrar un resumen del conjunto de datos
 st.write(df.describe())
 
-# Mostrar un gráfico de barras de la cantidad de visitas por tipo de sujeto
-st.bar_chart(df["tipo_clase_sujeto"].value_counts())
-
-# Mostrar un mapa de la cantidad de visitas por comuna
+# Mostrar un gráfico de barras de la cantidad de visitas por comuna
 st.map(df.groupby("comuna")["tipo_clase_sujeto"].value_counts())
 
 # Mostrar un gráfico de líneas de la cantidad de visitas por fecha
